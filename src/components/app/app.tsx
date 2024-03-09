96117ea7ed1c7511bf95267ec928bc1c0f170a18
Nikanorenkov-Mihail <mihanik03@gmail.com>
2024-03-09 14:58:42 +0300

    1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,55 @@
+import FavoritePage from '../../pages/favorites';
+import LoginPage from '../../pages/login';
 import MainPage from '../../pages/mainPage';
 
-type AppPageProps = {
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import OfferPage from '../../pages/offer';
+import Page404 from '../../pages/page404';
+
+ type AppPageProps = {
   placesCount: number;
 }
 
 function App({placesCount}: AppPageProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount} />
+   // <MainPage placesCount={placesCount} />
+   <BrowserRouter>
+   <Routes>
+     // '/url1' — подстрока, которая должна содержаться в адресной строке,
+     // чтобы был отображён компонент Page1
+     <Route
+       path='/'
+       element={<MainPage placesCount={placesCount}/>}
+     />
+     // '/url2' — подстрока, которая должна содержаться в адресной строке,
+     // чтобы был отображён компонент Page2
+     <Route
+       path='/login'
+       element={<LoginPage />}
+     />
+     ....
+     // '/urlN; — подстрока, которая должна содержаться в адресной строке,
+     // чтобы был отображён компонент PageN
+     <Route
+       path='/favorites'
+       element={<FavoritePage />}
+     />
+      <Route
+       path='/offer/:id'
+       element={<OfferPage />}
+     />
+      <Route
+       path='*'
+       element={<Page404 />}
+     />
+   </Routes>
+ </BrowserRouter>
+
   );
 }
 
---
+++ src/pages/page404.tsx
@@ -0,0 +1,12 @@
+function Page404(): JSX.Element {
+    return (
+        <div>
+        <h1>Ошибка 404. Страница не существует.</h1>
+        <a
+         href="/">Back to main page
+         </a>
+         </div>
+    );
+  }
+  
+  export default Page404;
